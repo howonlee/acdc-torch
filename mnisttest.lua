341e8317938949777aabdf5129c5e39ac7f8603a
howon <hlee.howon@gmail.com>
2016-05-25 05:10:09 +0000

    forward is workingish, I think

--- mnisttest.lua
+++ mnisttest.lua
@@ -1,6 +1,7 @@
 require 'torch'
 require 'acdc'
 require 'nn'
+require 'math'
 local mnist = require 'mnist'
 
 mlp = nn.Sequential();
@@ -9,24 +10,21 @@ mlp:add(nn.Reshape(784))
 mlp:add(nn.Linear(inputs, HUs))
 mlp:add(nn.Tanh())
 mlp:add(acdc.ACDC(HUs))
-mlp:add(nn.Tanh())
-mlp:add(acdc.ACDC(HUs))
-mlp:add(nn.Tanh())
-mlp:add(acdc.ACDC(HUs))
-mlp:add(nn.Tanh())
-mlp:add(acdc.ACDC(HUs))
+-- mlp:add(nn.Linear(HUs, HUs))
 mlp:add(nn.Tanh())
 mlp:add(nn.Linear(HUs, outputs))
-mlp:add(nn.LogSoftMax())
-crit = nn.ClassNLLCriterion()
+crit = nn.MSECriterion()
 local trainset = mnist.traindataset()
 local testset = mnist.testdataset()
 
-for i = 1,50000 do
-  local input = trainset[i].x
-  local output= trainset[i].y
+for i = 1,40000 do
+  if math.fmod(i, 1000) == 0 then
+    print(i)
+  end
+  local input = trainset[i].x:double()
+  local output = torch.Tensor(10):double()
+  output[(trainset[i].y)+1] = 1.0
 
-  -- feed it to the neural network and the criterion
   crit:forward(mlp:forward(input), output)
 
   -- train over this example in 3 steps
@@ -40,9 +38,12 @@ end
 
 local total_acc = 0.0;
 for j = 1,5000 do
-  local curr_in = testset[j].x
-  local curr_out = testset[j].y
-  local res = mlp:forward(curr_in)
+  local curr_in = testset[j].x:double()
+  local curr_out = testset[j].y + 1
+  res = mlp.forward(curr_in)
+  _, res = torch.max(mlp:forward(curr_in), 1)
+  print(res)
+  print(curr_out)
   if res == curr_out then
     total_acc = total_acc + 1
   end
