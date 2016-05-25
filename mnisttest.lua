79284484ef9a022006305d4533b971f43389d93d
howon <hlee.howon@gmail.com>
2016-05-25 04:39:07 +0000

    write down some thoughts about possible mnist testing

--- mnisttest.lua
+++ mnisttest.lua
@@ -1,10 +1,11 @@
 require 'torch'
 require 'acdc'
 require 'nn'
-require 'mnist'
+local mnist = require 'mnist'
 
 mlp = nn.Sequential();
 inputs = 784; outputs = 10; HUs=200;
+mlp:add(nn.Reshape(784))
 mlp:add(nn.Linear(inputs, HUs))
 mlp:add(nn.Tanh())
 mlp:add(acdc.ACDC(HUs))
@@ -16,13 +17,14 @@ mlp:add(nn.Tanh())
 mlp:add(acdc.ACDC(HUs))
 mlp:add(nn.Tanh())
 mlp:add(nn.Linear(HUs, outputs))
-crit = nn.CrossEntropyCriterion()
+mlp:add(nn.LogSoftMax())
+crit = nn.ClassNLLCriterion()
+local trainset = mnist.traindataset()
+local testset = mnist.testdataset()
 
-for i = 1,2500 do
-  -- random sample
-  local input = mnist.something;
-  -- local input= torch.randn(2);     -- normally distributed example in 2d
-  local output= mnist.something;
+for i = 1,50000 do
+  local input = trainset[i].x
+  local output= trainset[i].y
 
   -- feed it to the neural network and the criterion
   crit:forward(mlp:forward(input), output)
@@ -36,4 +38,13 @@ for i = 1,2500 do
   mlp:updateParameters(0.01)
 end
 
-x = torch.Tensor(2) -- get the accuracy
+local total_acc = 0.0;
+for j = 1,5000 do
+  local curr_in = testset[j].x
+  local curr_out = testset[j].y
+  local res = mlp:forward(curr_in)
+  if res == curr_out then
+    total_acc = total_acc + 1
+  end
+end
+print(total_acc)
